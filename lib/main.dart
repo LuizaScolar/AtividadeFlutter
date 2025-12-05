6e438f5275b6f6bb34c64386ae8016ff8311bc45
Luiza Scolar <scolar441@gmail.com>
2025-12-05 15:16:13 -0300

    Finalizando Projeto

--- lib/main.dart
+++ lib/main.dart
@@ -2,112 +2,106 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(MyApp());
-
 }
 
-// atalho stless
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
   @override
-  Widget build(BuildContext context) 
-  {
+  Widget build(BuildContext context) {
     return MaterialApp(
-      // Desabilita banner debug
-      debugShowCheckedModeBanner: false,
+      debugShowCheckedModeBanner: false, // remove o banner de debug
+      theme: ThemeData(
+        primaryColor: Colors.deepPurple[900], // roxo escuro
+        scaffoldBackgroundColor: Colors.grey[850], // cinza escuro
+        textTheme: TextTheme(
+          bodyMedium: TextStyle(color: Colors.white), // texto branco
+        ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            foregroundColor: Colors.white, // texto do botão branco
+            backgroundColor: Colors.deepPurple[700], // fundo roxo escuro
+          ),
+        ),
+      ),
       home: HomePage(),
     );
   }
 }
 
-//atalho stless
-class HomePage extends StatelessWidget {
-  HomePage({super.key});
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
 
-  int contador =0;
+class _HomePageState extends State<HomePage> {
+  int contador = 0;
+  final int limite = 10;
 
-  void decremento(){
-    contador--;
-    print(contador);
+  bool get isZero => contador == 0;
+  bool get isFull => contador == limite;
+
+  void incrementar() {
+    if (!isFull) {
+      setState(() {
+        contador++;
+      });
+    }
   }
 
-  void incremento(){
-   contador++;
-  print(contador); 
+  void decrementar() {
+    if (!isZero) {
+      setState(() {
+        contador--;
+      });
+    }
   }
-  
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        decoration: BoxDecoration(
-          image: DecorationImage(image: AssetImage('assets/img/hellokitty.jfif'),
-          fit: BoxFit.cover,),
-        ),
-        child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text('Pode Entrar',
-            style: TextStyle(
-              fontSize: 26.0,
-              color: const Color.fromARGB(255, 120, 100, 152),
-              fontWeight: FontWeight.w700,
-            ),
-          ),
-           Padding(padding: EdgeInsetsGeometry.all(40),
-          child: Text(contador.toString(),
-             style: TextStyle(
-              fontSize: 26.0,
-              color: const Color.fromARGB(255, 121, 92, 148),
-              fontWeight: FontWeight.w700,
-             ),
-            ),
-          ),
-
-
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-                TextButton(onPressed:decremento,
-                  style:  TextButton.styleFrom(
-                    backgroundColor: const Color.fromARGB(255, 125, 108, 148),
-                    fixedSize: const Size(100,100),
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(24),
-                    ),
-                  ),
-                  child: Text('Sair',
-                    style:TextStyle(
-                      color:const Color.fromARGB(255, 255, 255, 255),
-                      fontSize: 16,
-                    )
-                  ),
-                ),
-                 
-              TextButton(onPressed: incremento,
-                style:  TextButton.styleFrom(
-                    backgroundColor: const Color.fromARGB(255, 169, 148, 188),
-                    fixedSize: const Size(100,100),
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(24),
-                    ),
-                ),
-                child: Text('Entrar',
-                  style:TextStyle(
-                    color:Colors.black,
-                    fontSize: 16,
-                  )
-                ),
-              ),
-
-            ],
-
-          )
-        ],
+      appBar: AppBar(
+        title: Text("Contador"),
+        backgroundColor: Colors.deepPurple[900],
       ),
-      )
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              isFull ? "Lotado" : "$contador",
+              style: TextStyle(
+                fontSize: 40,
+                color: Colors.white, // letras brancas
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(height: 20),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                ElevatedButton(
+                  onPressed: isZero ? null : decrementar,
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: isZero
+                        ? Colors.grey
+                        : Colors.deepPurple[700], // botão cinza se desativado
+                  ),
+                  child: Text("Decrementar"),
+                ),
+                SizedBox(width: 20),
+                ElevatedButton(
+                  onPressed: isFull ? null : incrementar,
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: isFull
+                        ? Colors.grey
+                        : Colors.deepPurple[700], // botão cinza se desativado
+                  ),
+                  child: Text("Incrementar"),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
-
